8615e0eb46780a4455a6d945a87c13da78c7eccf
ampatspell <ampatspell@gmail.com>
2016-10-05 01:57:29 +0300

    dummy

--- tests/dummy/app/routes/authors/new.js
+++ tests/dummy/app/routes/authors/new.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
+const {
+  RSVP: { hash }
+} = Ember;
+
 export default Ember.Route.extend({
 
   model() {
-    return this.get('store.db.main').model('author', { name: 'unnamed' });
+    return hash({
+      author: this.get('store.db.main').model('author', { name: 'unnamed' }),
+      blogs: this.get('store.db.main').find({ model: 'blog', selector: {} })
+    });
   }
 
 });
