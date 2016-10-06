8b823911a19f9dd7e3274fb8a7bf4226a86c21ad
ampatspell <ampatspell@gmail.com>
2016-10-07 00:25:37 +0300

    cleanup

--- app/initializers/sofa-couch.js
+++ app/initializers/sofa-couch.js
@@ -1,18 +1,18 @@
 import Couches from 'sofa/couch/couches';
 import Couch from 'sofa/couch/couch';
-import CouchSession from 'sofa/couch/session';
-import CouchRequest from 'sofa/couch/request';
-import CouchDatabases from 'sofa/couch/databases';
-import CouchDatabase from 'sofa/couch/database';
+import Request from 'sofa/couch/request';
+import Session from 'sofa/couch/session';
+import Databases from 'sofa/couch/databases';
+import Database from 'sofa/couch/database';
 
 export default {
   name: 'sofa:couch',
   initialize(container) {
     container.register('couch:couches', Couches, { instantiate: false });
     container.register('couch:main', Couch, { instantiate: false });
-    container.register('couch:request', CouchRequest, { instantiate: false });
-    container.register('couch:session', CouchSession, { instantiate: false });
-    container.register('couch:databases', CouchDatabases, { instantiate: false });
-    container.register('couch:database', CouchDatabase, { instantiate: false });
+    container.register('couch:request', Request, { instantiate: false });
+    container.register('couch:session', Session, { instantiate: false });
+    container.register('couch:databases', Databases, { instantiate: false });
+    container.register('couch:database', Database, { instantiate: false });
   }
 };
