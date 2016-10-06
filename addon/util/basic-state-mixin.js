91a499430c6df260a84644b30e333278d7055de9
ampatspell <ampatspell@gmail.com>
2016-10-07 02:50:44 +0300

    basic-state-mixin now can observe changes

--- addon/util/basic-state-mixin.js
+++ addon/util/basic-state-mixin.js
@@ -3,118 +3,129 @@ import Ember from 'ember';
 const {
   merge,
   copy,
-  computed
+  computed,
+  observer
 } = Ember;
 
-let props = {
-  isLoading: false,
-  isLoaded: false,
-  isDirty: false,
-  isSaving: false,
-  isError: false,
-  error: null,
-};
+export default function(opts) {
 
-let stateProperty = (prop) => {
-  return computed(function() {
-    return this.get('state')[prop];
-  }).readOnly();
-};
-
-let state = () => {
-  return computed(function() {
-    return copy(props);
-  }).readOnly();
-};
-
-let hash = {};
-
-for(let key in props) {
-  hash[key] = stateProperty(key);
-}
-
-let setter = (arg) => {
-  return function() {
-    if(this.isDestroying) {
-      return;
-    }
-
-    let hash;
-
-    if(typeof arg === 'function') {
-      hash = arg.call(this, ...arguments);
-    } else {
-      hash = arg;
-    }
-
-    this.beginPropertyChanges();
-    {
-      let state = this.get('state');
-      for(let key in state) {
-        if(state[key] !== hash[key]) {
-          state[key] = hash[key];
-          this.notifyPropertyChange(key);
-        }
-      }
-    }
-    this.endPropertyChanges();
-  };
-};
-
-export default Ember.Mixin.create(merge(hash, {
-
-  state: state(),
-
-  onDirty: setter({
-    isDirty: true
-  }),
-
-  onNew: setter({
+  let props = {
     isLoading: false,
     isLoaded: false,
     isDirty: false,
     isSaving: false,
     isError: false,
-    error: null
-  }),
+    error: null,
+  };
 
-  onLoading: setter({
-    isLoading: true,
-    isError: false,
-    error: null
-  }),
+  let stateProperty = (prop) => {
+    return computed(function() {
+      return this.get('state')[prop];
+    }).readOnly();
+  };
 
-  onLoaded: setter({
-    isLoading: false,
-    isLoaded: true,
-    isDirty: false,
-    isSaving: false,
-    isError: false,
-    error: null
-  }),
+  let state = () => {
+    return computed(function() {
+      return copy(props);
+    }).readOnly();
+  };
 
-  onSaving: setter({
-    isSaving: true,
-    isError: false,
-    error: null
-  }),
+  let hash = {};
 
-  onSaved: setter({
-    isLoading: false,
-    isLoaded: true,
-    isDirty: false,
-    isSaving: false,
-    isError: false,
-    error: null
-  }),
+  for(let key in props) {
+    hash[key] = stateProperty(key);
+  }
 
-  onError: setter(function(error) {
-    return {
-      isLoading: false,
-      isSaving: false,
-      isError: true,
-      error
+  let setter = (arg) => {
+    return function() {
+      if(this.isDestroying) {
+        return;
+      }
+
+      let hash;
+
+      if(typeof arg === 'function') {
+        hash = arg.call(this, ...arguments);
+      } else {
+        hash = arg;
+      }
+
+      this.beginPropertyChanges();
+      {
+        let state = this.get('state');
+        for(let key in state) {
+          if(hash.hasOwnProperty(key) && state[key] !== hash[key]) {
+            state[key] = hash[key];
+            this.notifyPropertyChange(key);
+          }
+        }
+      }
+      this.endPropertyChanges();
     };
-  })
+  };
 
-}));
+  for(let key in opts) {
+    let value = opts[key];
+    hash[`_observe_${key}_deps`] = observer(...value, function() {
+      this[key].call(this);
+    });
+  }
+
+  return Ember.Mixin.create(merge(hash, {
+
+    state: state(),
+
+    onDirty: setter({
+      isDirty: true
+    }),
+
+    onNew: setter({
+      isLoading: false,
+      isLoaded: false,
+      isDirty: false,
+      isSaving: false,
+      isError: false,
+      error: null
+    }),
+
+    onLoading: setter({
+      isLoading: true,
+      isError: false,
+      error: null
+    }),
+
+    onLoaded: setter({
+      isLoading: false,
+      isLoaded: true,
+      isDirty: false,
+      isSaving: false,
+      isError: false,
+      error: null
+    }),
+
+    onSaving: setter({
+      isSaving: true,
+      isError: false,
+      error: null
+    }),
+
+    onSaved: setter({
+      isLoading: false,
+      isLoaded: true,
+      isDirty: false,
+      isSaving: false,
+      isError: false,
+      error: null
+    }),
+
+    onError: setter(function(error) {
+      return {
+        isLoading: false,
+        isSaving: false,
+        isError: true,
+        error
+      };
+    })
+
+  }));
+}
