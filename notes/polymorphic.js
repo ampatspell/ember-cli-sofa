4305365b0068d5474a227e51e19022be4f084c68
ampatspell <ampatspell@gmail.com>
2016-10-22 21:48:56 +0300

    note fix

--- notes/polymorphic.js
+++ notes/polymorphic.js
@@ -1,7 +1,7 @@
 // `section: belongsTo('section')` where `section` has few implementations (document 'section:jxphpdcdt1xc' is expected to be 'section' not 'category'). `{ polymorphic: true}`
 
 category: belongsTo('section', { polymorphic: true });
-sections: hasMany('section', { query: 'foo-sections', polymorphic: true });
+sections: hasMany('section', { polymorphic: true });
 
 {
   _id: 'section:one',
