5b55f209953ebb22ff61c45143965b677d4dcc13
ampatspell <ampatspell@gmail.com>
2017-02-22 14:06:48 +0200

    event-source-test for 1.6 only

--- tests/unit/event-source-test.js
+++ tests/unit/event-source-test.js
@@ -1,32 +1,36 @@
-import { module, test, createStore, cleanup, wait } from '../helpers/setup';
+import { configurations, cleanup, wait } from '../helpers/setup';
 import Listener from 'sofa/couch/changes/event-source';
 
-let store;
-let db;
+configurations({ only: '1.6' }, ({ module, test, createStore }) => {
 
-function flush() {
-  store = createStore();
-  db = store.get('db.main');
-}
+  let store;
+  let db;
 
-module('event-source', () => {
-  flush();
-  return cleanup(store, [ 'main' ]);
-});
+  function flush() {
+    store = createStore();
+    db = store.get('db.main');
+  }
 
-test.skip('listen', assert => {
-  let docs = db.get('documents');
-  let listener = new Listener(`${docs.get('url')}/_changes?feed=eventsource&include_docs=true&since=now`);
-  listener.start();
-  console.log('started');
-  assert.equal(listener.started, true);
-  return wait().then(() => {
-    return docs.save({ _id: 'foo' });
-  }).then(() => {
-    console.log('saved');
-    return wait();
-  }).then(() => {
-    assert.equal(listener.open, true);
-    listener.stop();
+  module('event-source', () => {
+    flush();
+    return cleanup(store, [ 'main' ]);
   });
+
+  test('listen', assert => {
+    let docs = db.get('documents');
+    let listener = new Listener(`${docs.get('url')}/_changes?feed=eventsource&include_docs=true&since=now`);
+    listener.start();
+    console.log('started');
+    assert.equal(listener.started, true);
+    return wait().then(() => {
+      return docs.save({ _id: 'foo' });
+    }).then(() => {
+      console.log('saved');
+      return wait();
+    }).then(() => {
+      assert.equal(listener.open, true);
+      listener.stop();
+    });
+  });
+
 });
