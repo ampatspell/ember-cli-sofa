657dc38cd681d9a6cd5daef6debce5d7103cd4a4
ampatspell <ampatspell@gmail.com>
2017-06-03 19:19:05 +0300

    fix

--- blueprints/ember-cli-sofa/index.js
+++ blueprints/ember-cli-sofa/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {
   },
   afterInstall: function() {
-    this.addAddonToProject('ember-cli-couch', '0.0.2')
+    return this.addAddonToProject('ember-cli-couch', '0.0.2')
   }
 };
