ffe627dc3c3908c9f39dff78284e9cd4a4d906c1
ampatspell <ampatspell@gmail.com>
2017-06-05 03:18:12 +0300

    bump couch dependency

--- blueprints/ember-cli-sofa/index.js
+++ blueprints/ember-cli-sofa/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {
   },
   afterInstall: function() {
-    return this.addAddonToProject('ember-cli-couch', '0.0.6')
+    return this.addAddonToProject('ember-cli-couch', '0.0.7')
   }
 };
