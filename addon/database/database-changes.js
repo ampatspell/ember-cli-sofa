9eca267b628f7c678a22d3c819c7490c9fdec7b8
ampatspell <ampatspell@gmail.com>
2017-06-15 15:31:22 +0300

    database.changes with identity

---
+++ addon/database/database-changes.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import InternalChanges from '../changes/internal-changes';
+
+export default Ember.Mixin.create({
+
+  _createChangesForInternalChanges(internal) {
+    return this.get('store')._createChangesForInternalChanges(internal);
+  },
+
+  _changesClassForName(name) {
+    return this.get('store')._changesClassForName(name);
+  },
+
+  _createInternalChanges(changesClass, identifier, opts) {
+    return new InternalChanges(this, changesClass, identifier, opts);
+  },
+
+  _existingChanges(changesClass, opts, create=false) {
+    let identifier = this._changesIdentifier(changesClass, opts);
+    let internal = this._existingChangesForIdentifier(identifier);
+    if(!internal && create) {
+      internal = this._createInternalChanges(changesClass, identifier, opts);
+      this._onInternalChangesCreated(internal);
+    }
+    return internal;
+  },
+
+  changes(name, opts) {
+    let changesClass = this._changesClassForName(name);
+    let internal = this._existingChanges(changesClass, opts, true);
+    return internal.getChangesModel();
+  }
+
+});
---
+++ addon/database/database-internal-changes-identity.js
@@ -0,0 +1,48 @@
+import Ember from 'ember';
+import EmptyObject from '../util/empty-object';
+import { destroyObject } from '../util/destroy';
+
+const {
+  get,
+  on
+} = Ember;
+
+export default Ember.Mixin.create({
+
+  _createChangesIdentity: on('init', function() {
+    this._changesIdentity = new EmptyObject();
+  }),
+
+  _serializeChangesOpts(opts) {
+    return JSON.stringify(opts);
+  },
+
+  _changesIdentifier(changesClass, opts) {
+    if(!opts) {
+      opts = null;
+    }
+    let modelName = get(changesClass, 'modelName');
+    let serializedOpts = this._serializeChangesOpts(opts);
+    return `${modelName} ${serializedOpts}`;
+  },
+
+  _existingChangesForIdentifier(identifier) {
+    return this._changesIdentity[identifier];
+  },
+
+  _onInternalChangesCreated(changes) {
+    let identifier = changes.identifier;
+    this._changesIdentity[identifier] = changes;
+    return changes;
+  },
+
+  _onInternalChangesDestroyed(changes) {
+    let identifier = changes.identifier;
+    delete this._changesIdentity[identifier];
+  },
+
+  _destroyInternalChangesIdentity() {
+    destroyObject(this._changesIdentity);
+  },
+
+});
