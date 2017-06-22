c55416026ac679aaaaacc8c56c986cb70cf65edd
ampatspell <ampatspell@gmail.com>
2017-06-22 21:45:35 +0300

    all relation proxies now supports mixins
    ``` javascript
    Model.extend({
      foo: hasMany('bar', { ..., relationship: 'foo-bars' })
    })
    
    // app/relationships/foo-bars.js
    export default Relationship.extend({ ... })
    ```

--- addon/store/store-relation.js
+++ addon/store/store-relation.js
@@ -6,24 +6,49 @@ import {
 
 export default Ember.Mixin.create({
 
-  _relationProxyClassForName(proxyName, variant) {
+  __relationProxyClassForName(proxyName, variant, variantFn) {
     return this._classForName(`sofa:${proxyName}`, 'base', variant, Proxy => {
       let Extended = Proxy.extend();
       Extended.reopenClass({ store: this, [__sofa_type__]: __sofa_relation_proxy_type__ });
       return Extended;
-    });
+    }, variantFn);
+  },
+
+  _relationshipMixinBuilderForName(name) {
+    if(!name) {
+      return;
+    }
+    return this._classForName('relationship', name);
+  },
+
+  _buildRelationshipMixin(relation) {
+    let variant = relation.relationship.opts.relationship;
+    let builder = this._relationshipMixinBuilderForName(variant);
+    let Mixin;
+    if(builder) {
+      Mixin = builder.build();
+    }
+    return {
+      variant,
+      Mixin
+    };
+  },
+
+  _relationProxyClassForName(_relation, proxyName) {
+    let { variant, Mixin } = this._buildRelationshipMixin(_relation);
+    return this.__relationProxyClassForName(proxyName, variant, Proxy => Proxy.reopen(Mixin));
   },
 
   _createBelongsToLoadedProxyForRelation(_relation) {
-    return this._relationProxyClassForName('belongs-to-loaded').create({ _relation });
+    return this._relationProxyClassForName(_relation, 'belongs-to-loaded').create({ _relation });
   },
 
   _createHasManyLoadedProxyForRelation(_relation, content) {
-    return this._relationProxyClassForName('has-many-loaded').create({ _relation, content });
+    return this._relationProxyClassForName(_relation, 'has-many-loaded').create({ _relation, content });
   },
 
   _createHasManyPersistedProxyForRelation(_relation, content) {
-    return this._relationProxyClassForName('has-many-persisted').create({ _relation, content });
+    return this._relationProxyClassForName(_relation, 'has-many-persisted').create({ _relation, content });
   }
 
 });
