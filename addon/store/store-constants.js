f22d4448be63cd70f993b3f54adcf6fed7e4d1ad
ampatspell <ampatspell@gmail.com>
2017-06-22 21:16:36 +0300

    use store._classForName to lookup proxy classes

--- addon/store/store-constants.js
+++ addon/store/store-constants.js
@@ -4,3 +4,4 @@ export const __sofa_model_type__ = 'model';
 export const __sofa_query_type__ = 'query';
 export const __sofa_collection_type__ = 'collection';
 export const __sofa_changes_type__ = 'changes';
+export const __sofa_relation_proxy_type__ = 'relation-proxy';
--- addon/store/store-relation.js
+++ addon/store/store-relation.js
@@ -1,24 +1,29 @@
 import Ember from 'ember';
-
-const {
-  getOwner
-} = Ember;
+import {
+  __sofa_type__,
+  __sofa_relation_proxy_type__
+} from './store-constants';
 
 export default Ember.Mixin.create({
 
+  _relationProxyClassForName(proxyName, variant) {
+    return this._classForName(`sofa:${proxyName}`, 'base', variant, Proxy => {
+      let Extended = Proxy.extend();
+      Extended.reopenClass({ store: this, [__sofa_type__]: __sofa_relation_proxy_type__ });
+      return Extended;
+    });
+  },
+
   _createBelongsToLoadedProxyForRelation(_relation) {
-    let owner = getOwner(this);
-    return owner.factoryFor('sofa:belongs-to-loaded').create({ _relation });
+    return this._relationProxyClassForName('belongs-to-loaded').create({ _relation });
   },
 
   _createHasManyLoadedProxyForRelation(_relation, content) {
-    let owner = getOwner(this);
-    return owner.factoryFor('sofa:has-many-loaded').create({ _relation, content });
+    return this._relationProxyClassForName('has-many-loaded').create({ _relation, content });
   },
 
   _createHasManyPersistedProxyForRelation(_relation, content) {
-    let owner = getOwner(this);
-    return owner.factoryFor('sofa:has-many-persisted').create({ _relation, content });
+    return this._relationProxyClassForName('has-many-persisted').create({ _relation, content });
   }
 
 });
