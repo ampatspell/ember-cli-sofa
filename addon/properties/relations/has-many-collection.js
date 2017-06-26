79f6c18364a415eb17d61d821ad8b1cec3553082
ampatspell <ampatspell@gmail.com>
2017-06-26 20:09:01 +0300

    experiments with relation

--- addon/properties/relations/has-many-collection.js
+++ addon/properties/relations/has-many-collection.js
@@ -1,4 +1,52 @@
-import HasManyLoadedRelation from './has-many-loaded';
+import Ember from 'ember';
+import Relation from './relation';
+import RelationLoader from './util/relation-loader';
+import QueryFindMixin from '../../util/query-find-mixin';
+import RelationQueryMixin from './query/relation';
+import RelationFindQueryMixin from './query/relation-find';
 
-export default class HasManyCollectionRelation extends HasManyLoadedRelation {
+const {
+  A
+} = Ember;
+
+export default class HasManyCollectionRelation extends Relation {
+
+  constructor() {
+    super(...arguments);
+    this.loader = new RelationLoader(this);
+  }
+
+  createArrayProxy() {
+    let content = this.getContent();
+    let store = this.relationship.store;
+    return store._createHasManyCollectionProxyForRelation(this, content);
+  }
+
+  createQuery() {
+    return this._createQuery({
+      name: 'relation-find',
+      prepare: Query => Query.extend(QueryFindMixin, RelationQueryMixin, RelationFindQueryMixin)
+    });
+  }
+
+  getContent() {
+    return A();
+  }
+
+  setValue() {
+  }
+
+  getValue() {
+    let value = this.value;
+    if(!value) {
+      value = this.createArrayProxy();
+      // value.addObserver('query', this, 'valueQueryDidChange');
+      this.value = value;
+    }
+    return value;
+  }
+
+  // destroyValue() {
+  // }
+
 }
