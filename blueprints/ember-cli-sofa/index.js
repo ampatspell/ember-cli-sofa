74467bc84798bbe3a2f63711377861da03998015
ampatspell <ampatspell@gmail.com>
2017-06-27 23:28:14 +0300

    update couch dep in blueprint

--- blueprints/ember-cli-sofa/index.js
+++ blueprints/ember-cli-sofa/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {
   },
   afterInstall: function() {
-    return this.addAddonToProject('ember-cli-couch', '0.0.11')
+    return this.addAddonToProject('ember-cli-couch', '0.0.13')
   }
 };
