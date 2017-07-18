518c10ceee35a2d46b715d5892f65602d2bd005a
ampatspell <ampatspell@gmail.com>
2017-07-18 23:55:30 +0300

    remove window.ops

--- addon/database/database-operations.js
+++ addon/database/database-operations.js
@@ -7,15 +7,12 @@ const operations = function() {
   });
 };
 
-window.ops = [];
-
 export default Ember.Mixin.create({
 
   operations: operations(),
 
   _registerOperation(name, subject, promise) {
     this.get('operations').register(name, subject, promise);
-    window.ops.push({ name, subject });
     return promise;
   },
 
